d2af4f1d512560f8338418a6a3406394aada4332
Samir Hadi <ultrakorne@gmail.com>
2024-12-03 19:41:00 +0100

    cleanup day 3

--- lib/y2024/day3.ex
+++ lib/y2024/day3.ex
@@ -1,18 +1,10 @@
 defmodule Aoc.Y2024.Day3 do
-  defp parse_input() do
+  defp parse_input(regex) do
     input =
       "data/2024/day3.txt"
       |> File.read!()
 
-    Regex.scan(~r/mul\((\d+,\d+)\)/, input)
-  end
-
-  defp parse_input_2() do
-    input =
-      "data/2024/day3.txt"
-      |> File.read!()
-
-    Regex.scan(~r/do\(\)|don't\(\)|mul\((\d+,\d+)\)/, input)
+    Regex.scan(regex, input)
   end
 
   defp get_int_array(str) do
@@ -20,7 +12,8 @@ defmodule Aoc.Y2024.Day3 do
   end
 
   def execute_1() do
-    parse_input()
+    ~r/mul\((\d+,\d+)\)/
+    |> parse_input()
     |> Enum.reduce(0, fn [_, y | _], acc ->
       [f, s | _] = get_int_array(y)
       acc + f * s
@@ -28,8 +21,9 @@ defmodule Aoc.Y2024.Day3 do
   end
 
   def execute_2() do
-    parse_input_2()
-    |> Enum.reduce({[], true}, fn e, {acc, to_mult} ->
+    ~r/do\(\)|don't\(\)|mul\((\d+,\d+)\)/
+    |> parse_input()
+    |> Enum.reduce({0, true}, fn e, {acc, to_mult} ->
       case e do
         ["do()"] ->
           {acc, true}
@@ -37,16 +31,14 @@ defmodule Aoc.Y2024.Day3 do
         ["don't()"] ->
           {acc, false}
 
-        [_, y] ->
-          if to_mult do
-            [f, s] = get_int_array(y)
-            {acc ++ [[f, s]], to_mult}
-          else
-            {acc, to_mult}
-          end
+        [_, y] when to_mult ->
+          [f, s] = get_int_array(y)
+          {acc + f * s, to_mult}
+
+        _ ->
+          {acc, to_mult}
       end
     end)
     |> elem(0)
-    |> Enum.reduce(0, fn [f, s], acc -> acc + f * s end)
   end
 end
