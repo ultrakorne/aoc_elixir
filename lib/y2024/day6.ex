f8f070654a74d04fdbc3026ea17526d810a5d02e
Samir Hadi <ultrakorne@gmail.com>
2024-12-14 12:34:25 +0100

    day 6, star 2

--- lib/y2024/day6.ex
+++ lib/y2024/day6.ex
@@ -60,58 +60,42 @@ defmodule Aoc.Y2024.Day6 do
 
   defp sum_vec({x1, y1}, {x2, y2}), do: {x1 + x2, y1 + y2}
 
-  defp prev_visited_in_line(map, {x, y}, dir, acc \\ []) do
-    cell = Map.get(map, {x, y})
+  defp rotate_until_free(map, pos, dir) do
+    next_pos = sum_vec(pos, guard_dir_to_vec(dir))
+    next_cell = Map.get(map, next_pos)
 
-    if cell == nil or cell.content == :obstructed do
-      ## TODO prob we have to rotate and continue because we could have a loop after an obstacle
-      acc
+    if next_cell != nil and next_cell.content == :obstructed do
+      rotate_until_free(map, pos, rotate_dir(dir))
     else
-      acc = cell.visited ++ acc
-      dir_vec = guard_dir_to_vec(dir)
-      next_pos = sum_vec({x, y}, dir_vec)
-      prev_visited_in_line(map, next_pos, dir, acc)
+      {dir, pos}
     end
   end
 
-  defp detect_loop_aux(map, {x, y}, dir) do
-    # TODO, next post should already consider if there is an obstacle and rotate?
-    next_pos = sum_vec({x, y}, guard_dir_to_vec(dir))
+  defp detect_loop_aux(map, {x, y}, dir, count \\ 0) do
+    {new_dir, pos} = rotate_until_free(map, {x, y}, dir)
+    next_pos = sum_vec(pos, guard_dir_to_vec(new_dir))
     next_cell = Map.get(map, next_pos)
 
-    {dir, next_pos, next_cell} =
-      if next_cell.content == :obstructed do
-        new_dir = rotate_dir(dir)
-        next_pos = sum_vec({x, y}, guard_dir_to_vec(new_dir))
-        next_cell = Map.get(map, next_pos)
-        {new_dir, next_pos, next_cell}
-      else
-        {dir, next_pos, next_cell}
-      end
+    current_cell = Map.get(map, {x, y})
 
     cond do
-      next_cell == nil ->
-        0
-
-      Enum.member?(next_cell.visited, dir) ->
+      Enum.member?(current_cell.visited, new_dir) ->
         1
 
-      true ->
-        # rotate
-        new_dir = if next_cell.content == :obstructed, do: rotate_dir(dir), else: dir
-        # update map with new direction
-        current_cell = Map.get(map, {x, y})
+      next_cell == nil ->
+        # map is exited, no loop
+        0
 
+      true ->
+        # update map with new direction
         updated_cell = %Cell{
           current_cell
           | visited: [new_dir | current_cell.visited],
             guard_dir: :none
         }
 
-        map = Map.put(map, guard_pos, guard_cell)
-        detect_loop_aux(map, next_pos, new_dir)
-
-        # call recursv
+        map = Map.put(map, {x, y}, updated_cell)
+        detect_loop_aux(map, next_pos, new_dir, count + 1)
     end
   end
 
@@ -122,27 +106,13 @@ defmodule Aoc.Y2024.Day6 do
     rock_cell = Map.get(map, rock_placement)
     # if the cell where we want to place the rock was visited in the past, we cannot place a rock
     if rock_cell == nil or rock_cell.content == :obstructed or rock_cell.visited != [] do
-      IO.puts("Cannot place rock because next cell was visited, is nill or has already a rock")
       0
     else
-      guard_rotated = rotate_dir(dir)
       updated_rock_cell = %Cell{rock_cell | content: :obstructed}
-      map = Map.put(map, rock_placement, updated_rock_cell)
+      loop_test_map = Map.put(map, rock_placement, updated_rock_cell)
 
-      has_loop = detect_loop_aux(map, guard_pos, guard_rotated)
-      has_loop
+      detect_loop_aux(loop_test_map, guard_pos, dir)
     end
-
-    # guard_rotated = rotate_dir(dir)
-    # visited_line = prev_visited_in_line(map, guard_pos, guard_rotated)
-
-    # if Enum.member?(visited_line, guard_rotated) do
-    #   placing_rock = sum_vec(dir_vec, guard_pos)
-    #   IO.inspect(placing_rock, label: "Placing rock at")
-    #   1
-    # else
-    #   0
-    # end
   end
 
   defp walk_map(map, guard_pos, loops \\ 0) do
@@ -160,17 +130,14 @@ defmodule Aoc.Y2024.Day6 do
 
     cond do
       next_guard_cell == nil ->
-        # IO.inspect("Guard is out!")
         {map, loops}
 
       next_guard_cell.content == :obstructed ->
-        # IO.inspect("Obstructed!")
         guard_cell = %Cell{guard_cell | guard_dir: rotate_dir(dir)}
         map = Map.put(map, guard_pos, guard_cell)
         walk_map(map, guard_pos, loops)
 
       true ->
-        # IO.inspect("Moving guard")
         next_guard_cell = %Cell{next_guard_cell | guard_dir: dir}
         map = Map.put(map, new_pos_guard, next_guard_cell)
         walk_map(map, new_pos_guard, loops)
