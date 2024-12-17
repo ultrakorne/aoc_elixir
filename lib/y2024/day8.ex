f890ae9a6a2888a6432059206832a86cdf97a4ba
Samir Hadi <ultrakorne@gmail.com>
2024-12-17 14:39:10 +0100

    working on day8, start 2

--- lib/y2024/day8.ex
+++ lib/y2024/day8.ex
@@ -10,7 +10,7 @@ defmodule Aoc.Y2024.Day8 do
     |> Enum.group_by(fn {_, value} -> value end, fn {key, _} -> key end)
   end
 
-  def find_antinodes_list(lst, acc \\ []) when is_list(lst) do
+  def find_antinodes_list(lst, bounds, acc \\ []) when is_list(lst) do
     case lst do
       [_] ->
         acc
@@ -18,59 +18,72 @@ defmodule Aoc.Y2024.Day8 do
       [c1 | rest] ->
         new_acc =
           rest
-          |> Enum.flat_map(fn c2 -> find_antinodes(c1, c2) end)
+          |> Enum.flat_map(fn c2 -> find_antinodes(c1, c2, bounds) end)
 
-        find_antinodes_list(rest, new_acc ++ acc)
+        find_antinodes_list(rest, bounds, new_acc ++ acc)
     end
   end
 
-  @spec find_antinodes({integer(), integer()}, {integer(), integer()}) :: [{integer(), integer()}]
-  def find_antinodes(c1, c2) do
-    [find_antinode(c1, c2), find_antinode(c2, c1)]
+  @spec find_antinodes({integer(), integer()}, {integer(), integer()}, {integer(), identifier()}) ::
+          [{integer(), integer()}]
+  def find_antinodes(c1, c2, bounds) do
+    right_side = find_antinode(c1, c2, bounds)
+    left_side = find_antinode(c2, c1, bounds)
+
+    [right_side, left_side]
+    |> Enum.reject(&is_nil/1)
   end
 
-  defp find_antinode(c1, c2) do
+  def find_antinodes_2(c1, c2, bounds) do
+    right_side = find_antinodes_line(c1, c2, bounds)
+    all_antinodes = find_antinodes_line(c2, c1, bounds, right_side)
+    all_antinodes
+  end
+
+  defp find_antinodes_line(c1, c2, bounds, acc \\ []) do
+    case find_antinode(c1, c2, bounds) do
+      nil -> acc
+      n -> find_antinodes_line(c2, n, bounds, [n | acc])
+    end
+  end
+
+  defp find_antinode(c1, c2, bounds) do
     d1 = sub_vec(c1, c2)
-    sub_vec(c2, d1)
+    result = sub_vec(c2, d1)
+    if check_out_of_bound(result, bounds), do: nil, else: result
   end
 
   defp check_out_of_bound({x, y}, {max_x, max_y}) do
     x < 0 or x > max_x or y < 0 or y > max_y
   end
 
-  defp remove_out_of_bound(lst, grid) do
-    max_bounds = grid |> grid_size()
-    Enum.reject(lst, fn {x, y} -> check_out_of_bound({x, y}, max_bounds) end)
-  end
-
   def execute_1() do
     grid = parse_grid("data/2024/day8.txt")
+    bounds = grid_size(grid)
     signal_map = grid |> frequency_map()
 
     signal_map
     |> Map.keys()
     |> Enum.reduce([], fn k, acc ->
       signal_nodes = Map.get(signal_map, k)
-      find_antinodes_list(signal_nodes, acc)
+      find_antinodes_list(signal_nodes, bounds, acc)
     end)
     |> Enum.uniq()
-    |> remove_out_of_bound(grid)
     |> Enum.count()
   end
 
   def execute_2() do
     grid = parse_grid("data/2024/day8_test.txt")
-    {max_x, max_y} = grid_size(grid)
+    bounds = grid_size(grid)
     signal_map = grid |> frequency_map()
 
     signal_map
     |> Map.keys()
     |> Enum.reduce([], fn k, acc ->
       signal_nodes = Map.get(signal_map, k)
-      find_antinodes_list(signal_nodes, acc)
+      find_antinodes_list(signal_nodes, bounds, acc)
     end)
     |> Enum.uniq()
-    |> remove_out_of_bound(grid)
     |> Enum.count()
   end
 end
--- test/y2024/day_8_test.exs
+++ test/y2024/day_8_test.exs
@@ -5,7 +5,8 @@ defmodule Aoc.Y2024.Day8Test do
   test "find antinodes" do
     c1 = {3, 4}
     c2 = {5, 5}
-    antinodes = Day8.find_antinodes(c1, c2)
+    bounds = {10, 10}
+    antinodes = Day8.find_antinodes(c1, c2, bounds)
     assert length(antinodes) == 2
     assert Enum.member?(antinodes, {1, 3})
     assert Enum.member?(antinodes, {7, 6})
@@ -13,7 +14,17 @@ defmodule Aoc.Y2024.Day8Test do
 
   test "finding antinodes of a list" do
     list = [{5, 6}, {8, 8}, {9, 9}]
-    antinodes = Day8.find_antinodes_list(list)
-    IO.inspect(antinodes, label: "antinodes list")
+    bounds = {20, 20}
+    antinodes = Day8.find_antinodes_list(list, bounds)
+    assert length(antinodes) == 6
+  end
+
+  test "antinodes in line for day 2" do
+    c1 = {3, 3}
+    c2 = {4, 4}
+    bound = {7, 7}
+
+    antinodes = Day8.find_antinodes_2(c1, c2, bound)
+    IO.inspect(antinodes, label: "antinodes d2")
   end
 end
