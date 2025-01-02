5763ce8960e050fc0392364845c2f8ab77b2573f
Samir Hadi <ultrakorne@gmail.com>
2025-01-02 18:42:57 +0100

    day 15 star 1

--- lib/y2024/day15.ex
+++ lib/y2024/day15.ex
@@ -43,10 +43,86 @@ defmodule Aoc.Y2024.Day15 do
     {grid, Enum.join(moves)}
   end
 
+  def find_robot(grid) do
+    grid
+    |> Map.keys()
+    |> Enum.find(fn coord -> Map.get(grid, coord) == "@" end)
+  end
+
+  def get_grid_slice(grid, {x, y}, dir, acc \\ "") do
+    result = Map.get(grid, {x, y})
+
+    case result do
+      nil ->
+        acc
+
+      r ->
+        next_pos = move_coord({x, y}, dir)
+        get_grid_slice(grid, next_pos, dir, acc <> r)
+    end
+  end
+
+  defp move_coord({x, y}, dir) do
+    case dir do
+      "<" -> {x - 1, y}
+      ">" -> {x + 1, y}
+      "^" -> {x, y - 1}
+      "v" -> {x, y + 1}
+    end
+  end
+
+  def replace_grid_slice(grid, {x, y}, dir, slice) do
+    graphemes = String.graphemes(slice)
+    replace_grid_slice_aux(grid, {x, y}, dir, graphemes)
+  end
+
+  defp replace_grid_slice_aux(grid, {x, y}, dir, slice_list) do
+    case slice_list do
+      [] ->
+        grid
+
+      [char | rest] ->
+        grid = Map.put(grid, {x, y}, char)
+        next_pos = move_coord({x, y}, dir)
+        replace_grid_slice_aux(grid, next_pos, dir, rest)
+    end
+  end
+
+  def move_push({grid, dir}) do
+    dir_list = String.graphemes(dir)
+    robot_pos = find_robot(grid)
+    move_push_aux(grid, robot_pos, dir_list)
+  end
+
+  defp move_push_aux(grid, robot_pos, dir_list) do
+    case dir_list do
+      [] ->
+        grid
+
+      [dir | rest] ->
+        slice = get_grid_slice(grid, robot_pos, dir)
+        new_slice = move_robot(slice)
+        new_grid = replace_grid_slice(grid, robot_pos, dir, new_slice)
+        robot_pos = find_robot(new_grid)
+        move_push_aux(new_grid, robot_pos, rest)
+    end
+  end
+
+  def box_scores(grid) do
+    grid
+    |> Map.keys()
+    |> Enum.filter(fn {x, y} -> Map.get(grid, {x, y}) == "O" end)
+    |> Enum.reduce(0, fn {x, y}, acc ->
+      acc + 100 * y + x
+    end)
+  end
+
   def execute_1() do
-    "data/2024/day15_test.txt"
+    "data/2024/day15.txt"
     |> File.read!()
     |> parse_grid()
+    |> move_push()
+    |> box_scores()
   end
 
   def execute_2() do
--- test/y2024/day15_test.exs
+++ test/y2024/day15_test.exs
@@ -43,4 +43,40 @@ defmodule Aoc.Y2024.Day15Test do
     out = Day15.move_robot(str)
     assert out == ".@OOO..#"
   end
+
+  test "grid slice" do
+    {grid, _} = "data/2024/day15_test.txt" |> File.read!() |> Day15.parse_grid()
+    robot_pos = Day15.find_robot(grid)
+    assert robot_pos == {2, 2}
+
+    grid_slice = Day15.get_grid_slice(grid, robot_pos, "<")
+    assert grid_slice == "@##"
+
+    grid_slice = Day15.get_grid_slice(grid, robot_pos, "^")
+    assert grid_slice == "@.#"
+
+    grid_slice = Day15.get_grid_slice(grid, robot_pos, ">")
+    assert grid_slice == "@.O..#"
+
+    grid_slice = Day15.get_grid_slice(grid, robot_pos, "v")
+    assert grid_slice == "@.#..#"
+  end
+
+  test "replace slice" do
+    {grid, _} = "data/2024/day15_test.txt" |> File.read!() |> Day15.parse_grid()
+    new_grid = Day15.replace_grid_slice(grid, {2, 2}, "<", "@.#")
+
+    assert Map.get(new_grid, {2, 2}) == "@"
+    assert Map.get(new_grid, {1, 2}) == "."
+    assert Map.get(new_grid, {0, 2}) == "#"
+  end
+
+  test "move push" do
+    {grid, _} = "data/2024/day15_test.txt" |> File.read!() |> Day15.parse_grid()
+    new_grid = Day15.move_push(grid, "^")
+
+    assert Map.get(new_grid, {2, 2}) == "."
+    assert Map.get(new_grid, {2, 1}) == "@"
+    assert Map.get(new_grid, {2, 0}) == "#"
+  end
 end
